1623c98428675ab165fbaf485bbaf552c35c4a65
pehor9164 <pehor9164@7KWN6Y1.Student.UGDSB.ED>
2018-09-18 09:42:21 -0400

    modified

--- src/netpay/NetPay.java
+++ src/netpay/NetPay.java
@@ -1,14 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Peter Horne-Deus
+ * NetPay.java
+ * This program calculates the netpay of a person making $5 per hour
+ * September 18, 2018
  */
-
 package netpay;
 
 /**
  *
- * @author Pehor9164
+ * @author Peter
  */
 public class NetPay {
 
@@ -17,6 +17,13 @@ public class NetPay {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double h = 40;
+        double w = 5;
+        double i = -2;
+        double t = 0.22;
+        double n =(h*w-i)-t*(h*w-i);
+        
+        System.out.println("The Net Pay of the person is " + n );
     }
     
 }
